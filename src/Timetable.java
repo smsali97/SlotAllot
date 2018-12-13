68ef69dbb0af42a7bdeb1930f7a09af19d001c80
Sualeh Ali <smsali97@gmail.com>
2018-12-13 13:12:47 +0500

    Added Timetable

--- src/Timetable.java
+++ src/Timetable.java
@@ -1,1 +1,15 @@
+import java.util.HashMap;
 import java.util.List;
+
+public class Timetable {
+	HashMap<String, Slot[]> schedule;
+	String name;
+	public static String[] daysOftheWeek= {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+	int timeInterval;
+	Creator creator;
+	List<Collaborator> collaborators;
+	String forWhom;
+	
+	public Timetable compareTo(Timetable t) { return null; }
+	public Timetable extractData(String s) { return null; }
+}
